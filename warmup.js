fc26538e3153c65e6a9ac29254f198dccbe79482
mash-a <mariya.emv@gmail.com>
2018-02-16 10:22:02 -0500

    created obj as applePie

--- warmup.js
+++ warmup.js
@@ -1,4 +1,11 @@
 var hobbies = ['photography', 'biking', 'hiking'];
 hobbies.push('writing', 'painting', 'running', 'filmmaking', 'sculpting', 'calligraphy');
+console.log(hobbies, 'pushed six items');
 hobbies.shift();
+console.log(hobbies, 'used shift');
 hobbies.pop();
+console.log(hobbies, 'pop');
+hobbies.splice(0,1);
+console.log(hobbies, 'used splice(0,1)');
+
+var applePie = {};
